9ada4c95f89fef0e863b5cd9497ae73cef85f911
Fırat Özden <31664778+fir4tozden@users.noreply.github.com>
2022-04-11 03:01:54 +0300

    Delete Debugger.js

--- src/handlers/Debugger.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    log: o => {
-        console.log("\x1b[36mpeakdb \x1b[34m» \x1b[32m" + o + "\x1b[0m")
-    }
-    , error: o => {
-        console.error("\x1b[36mpeakdb \x1b[34m» \x1b[31m" + o + "\x1b[0m")
-    }
-};
